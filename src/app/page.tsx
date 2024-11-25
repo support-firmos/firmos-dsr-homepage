b810af586afa25cae8228e32b711d34c04b78771
Keanu Reaño <reanokeanu@gmail.com>
2024-11-25 12:32:43 +0800

    downgraded nextjs to version 14 & refactored code to match SDK version.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,43 +1,31 @@
-// src/app/page.tsx
-
-"use client";
-
 import { TokenGate } from '@/components/TokenGate';
 import Footer from '@/components/Footer';
 import { LandingPage } from '@/components/landing-page';
-import { Suspense } from 'react';
-import { useSearchParams } from 'next/navigation';
+import { getSession } from '@/utils/session';
 
-// Page Component
-export default function Page() {
-  const searchParams = useSearchParams();
+async function Content({ searchParams }: { searchParams: SearchParams }) {
+  const data = await getSession(searchParams);
+  const displayName = data.client?.givenName || 'Guest';
 
   return (
+    <main>
+      <h1 className="absolute top-5 left-5 z-[9999] bg-[#14151A] text-white p-2.5 rounded-md text-lg font-sans shadow-md w-auto">
+        Welcome! <code>{displayName}</code>
+      </h1>
+      <LandingPage />
+      <Footer />
+    </main>
+  );
+}
+
+export default async function Page({
+  searchParams,
+}: {
+  searchParams: SearchParams;
+}) {
+  return (
     <TokenGate searchParams={searchParams}>
-      <Suspense fallback={<div>Loading...</div>}>
-        <main>
-          <h1
-            className="
-              absolute 
-              top-5 
-              left-5 
-              z-[9999] 
-              bg-[#14151A] 
-              text-white 
-              p-2.5 
-              rounded-md 
-              text-lg 
-              font-sans 
-              shadow-md 
-              w-auto
-            "
-          >
-            Welcome! <code>{searchParams.get('token') || 'Guest'}</code>
-          </h1>
-          <LandingPage />
-          <Footer />
-        </main>
-      </Suspense>
+      <Content searchParams={searchParams} />
     </TokenGate>
   );
 }
